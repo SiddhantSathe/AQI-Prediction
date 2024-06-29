6cdfc411f00687cdbca07d4521b3e7dc954801ec
Siddhant Sathe <sathesiddhant21@gmail.com>
2024-06-29 13:09:51 +0530

    used ozon3

---
+++ venv/Lib/site-packages/ozon3/urls.py
@@ -0,0 +1,43 @@
+"""urls module for the Ozon3 package.
+
+This module contains the URLs dataclass for the package.
+
+It should only be used with the Ozon3 package and not run directly.
+"""
+
+from dataclasses import dataclass
+
+
+# This prevents the data attributes in the class from being modified.
+# This is to ensure that a user/method doesn't accidentally modify the
+# endpoint urls stored here.
+@dataclass(frozen=True)
+class URLs:
+    """Class that contains the endpoint urls for the WAQI API
+
+    This class should not be instantiated. It only contains class level attributes,
+    and no methods at all. It is a static dataclass.
+
+    Attributes:
+        search_aqi_url (str): The endpoint used for retrieving air quality data.
+        find_stations_url (str): The endpoint used for
+         retrieving a collection of air quality measuring stations.
+        find_coordinates_url (str): The endpoint used for
+         retrieving geographical information
+    """
+
+    # Base API endpoint.
+    _base_url: str = "https://api.waqi.info/"
+
+    # For air quality data search by location.
+    search_aqi_url: str = f"{_base_url}feed/"
+
+    # For search for air quality measuring stations in area.
+    find_stations_url: str = f"{_base_url}search/"
+
+    # For Map Queries
+    find_coordinates_url: str = f"{_base_url}map/"
+
+
+if __name__ == "__main__":
+    pass
